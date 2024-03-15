ad0f4f352c91309b40a9605c049efdbce9243a3e
MariaCaroline2023 <163418693+MariaCaroline2023@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src="https://cdns-images.dzcdn.net/images/artist/ddca227df43e23406e9f66bbe41eba28/500x500.jpg"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src="https://static.wikia.nocookie.net/backyardigans/images/9/9d/The_backyardigans.jpg/revision/latest?cb=20200828182459&path-prefix=es"
+}
